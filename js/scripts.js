ca55e77166d0904e3646123af01273a403ffc497
Ermeson Ramos de Lima <samarasales2556@gmail.com>
2024-09-10 08:32:19 -0300

    inicio

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const informationsOfDataBase = [{
+    name: "Tecnico em informática",
+    areaDoCurso: "Tecnologia"
+},
+{
+    name: "Técnico em comércio",
+    areaDoCurso: "Negócios"
+},
+{
+    name: "Tecnico em Enfermagem",
+    areaDoCurso: "Tecnologia"
+},
+{
+    name: "Tecnico em informática",
+    areaDoCurso: "Tecnologia"
+}]
